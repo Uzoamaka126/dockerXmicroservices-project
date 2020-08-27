9013581a03b8a23f56d13f70f092ee946e098f1e
Uzoamaka126 <vanessapeters126@gmail.com>
2020-08-27 01:23:53 +0100

    make changes to the project

---
+++ set_env.sh
@@ -0,0 +1,13 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=amaka
+export POSTGRES_PASSWORD=expression12
+export POSTGRES_HOST=cdr-db.cd6dczwq137v.us-east-2.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=udacity-microservices-975829480648-dev
+export AWS_REGION=us-east-2a
+export AWS_PROFILE=default
+export JWT_SECRET=hello
+export URL=http://localhost:8100
+export DOCKER_USERNAME=26121226
+export DOCKER_PASSWORD=expression12
